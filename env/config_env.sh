ed504847c6cd9c198d24a5e4fae94fdcc94415be
Ubuntu <ubuntu@ip-172-31-29-104.ap-northeast-1.compute.internal>
2021-09-20 09:24:51 +0000

    beta

--- env/config_env.sh
+++ env/config_env.sh
@@ -8,13 +8,13 @@ cp ./.vimrc ~/
 cp -r ./.aws ~/
 
 # install miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/miniconda3"
-rm Miniconda3-latest-Linux-x86_64.sh
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#bash Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/miniconda3"
+#rm Miniconda3-latest-Linux-x86_64.sh
 
 ~/miniconda3/bin/conda init bash
-export PATH="/home/ubuntu/miniconda3/bin:$PATH"
-export s3_temp='s3://kyoto-shi-photos/group_share/li/temp/'
+#export PATH="/home/ubuntu/miniconda3/bin:$PATH"
+export s3='s3://kyoto-shi-photos/group_share/result0916/'
 
 # install aws-cli for conda base
 # conda install -n base nodejs=14.7 -y -c conda-forge
